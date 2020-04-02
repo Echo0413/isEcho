5f31a1824bbed750d62bea8f375a42bba2d36a78
Echo0413 <61871956+Echo0413@users.noreply.github.com>
2020-04-02 20:24:27 +0800

    Add files via upload

---
+++ JS/jajs2.js
@@ -0,0 +1,13 @@
+// JavaScript Document
+function is_changeimgh(){
+	imgid1=document.getElementById('abefore');
+	imgid1.src='image/abefore.PNG';
+	imgid1=document.getElementById('bbefore');
+	imgid1.src='image/bbefore.PNG';
+}
+function is_changeimgk(){
+	imgid1=document.getElementById('abefore');
+	imgid1.src='image/aafter.PNG';
+	imgid1=document.getElementById('bbefore');
+	imgid1.src='image/bafter.PNG';
+}
